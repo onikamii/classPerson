56fe530d12971b23cb3aa8c6137b1cdc45857e29
User <User@DESKTOP-SNB119S>
2023-02-05 22:01:00 +0300

    Добавьте файлы проекта.

---
+++ classPerson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace classPerson
+{
+    class Person
+    {
+        public string fullName;
+        public int age;
+        public Person(string fullName, int age)
+        {
+            this.fullName = fullName;
+            this.age = age;
+        }
+        public Person()
+        {
+
+        }
+        public void move(string fullName)
+        {
+            Console.WriteLine(fullName, "двигается");
+        }
+        public void talk(string fullName)
+        {
+            Console.WriteLine(fullName, "говорит");
+        }
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("Oleg", 23);
+            person2.move("Oleg");
+            person2.talk("Mihail");
+        }      
+    }
+}
